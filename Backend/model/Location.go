1f3489ae0dd0b73639616636c9c0df467b044674
unknown <dzonik125@gmail.com>
2023-03-14 20:13:49 +0100

    integration test for userRepo

--- Backend/model/Location.go
+++ Backend/model/Location.go
@@ -1,9 +1,6 @@
 package model
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
 type Location struct {
-	ID      primitive.ObjectID `bson:"_id,omitempty"`
-	Country string             `bson:"country,omitempty"`
-	City    string             `bson:"city,omitempty"`
+	Country string `bson:"country,omitempty"`
+	City    string `bson:"city,omitempty"`
 }
